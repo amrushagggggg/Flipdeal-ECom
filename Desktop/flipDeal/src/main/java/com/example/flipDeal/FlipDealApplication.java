083cfb849df5895bafd886fa7ebeba92fd3ae9f6
amrushagggggg <amrushagurjar1818@gmail.com>
2024-03-14 22:38:02 +0530

    first flipdeal commit

---
+++ Desktop/flipDeal/src/main/java/com/example/flipDeal/FlipDealApplication.java
@@ -0,0 +1,21 @@
+package com.example.flipDeal;
+
+import com.example.flipDeal.client.FlipDealClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class
+FlipDealApplication {
+	public static void main(String[] args) throws IOException {
+
+
+		SpringApplication.run(FlipDealApplication.class, args);
+
+
+	}
+
+
+}
---
+++ Desktop/flipDeal/src/main/java/com/example/flipDeal/dto/Discount.java
@@ -0,0 +1,12 @@
+package com.example.flipDeal.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Discount {
+  private  String discount_tag;
+  private double amount;
+
+}
